565407f70850ea5b6d3123775456cfe9e4c58fd4
Davide Spataro <d.spataro@mat.unical.it>
2016-02-13 17:31:19 +0000

    Added misc interview test

---
+++ src/Miscellanous/misc.h
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+//-----------------------------------------------------------------------------
+/*
+QUESTION:
+Given a time (Hours:Minutes) compute the angle between the two clock hands
+*/
+
+
+TEST(misc,angleBetweenHoursMinutes){
+	ASSERT_EQ(1,1);
+}
+
+//-----------------------------------------------------------------------------
+
+/*
+QUESTION:
+Given a time (Hours:Minutes:Seconds) compute the angle between the two clock hands
+taking in consideration the seconds's hand too
+*/
+
+TEST(LinkedList,angleBetweenHoursMinutesSeconds){
+	ASSERT_EQ(1,1);
+}
+
+
+
+//-----------------------------------------------------------------------------

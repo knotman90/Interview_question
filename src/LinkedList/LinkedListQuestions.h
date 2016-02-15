391cee62b3122e1eb5c27c02be0f3e2c83868ecc
Davide Spataro <d.spataro@mat.unical.it>
2016-02-15 21:36:02 +0000

    added linked list INTERSECTION Naiveh

--- src/LinkedList/LinkedListQuestions.h
+++ src/LinkedList/LinkedListQuestions.h
@@ -11,7 +11,7 @@ void initializeList(int size, int start, DSL::SingleLinkedList<int> &L1){
 		L1.append(start+i);
 	}
 }
-
+//----------------------------------------------------------------------------
 /*
 Given two list of the same type produce a third list
 that is the union of the other two. Duplicate elements should appear
@@ -91,13 +91,80 @@ void testUnion(const int SIZE, UnionFunction uf ){
 
 }
 TEST(LinkedList,LinkedList_UNION_naive){
-	testUnion(10000, listUnioneNaive<int>);
+	testUnion(1000, listUnioneNaive<int>);
 }
 TEST(LinkedList,LinkedList_UNION_smarter){
-	testUnion(10000, listUnioneSmarter<int>);
+	testUnion(1000, listUnioneSmarter<int>);
 }
 
 
-TEST(LinkedList,LinkedList_INTERSECTION){
-	ASSERT_EQ(1,1);
+//----------------------------------------------------------------------------
+/*
+Given two list of the same type produce a third list
+which elements make up the INTERSECTION between of the other two.
+Duplicate elements should appear only once in the final list.
+*/
+
+
+/**
+Complexity: Time O(|L1|^2 + |L2|^2)
+			Space O(1)
+*/
+template<class T>
+void listIntersectionNaive(DSL::SingleLinkedList<T> &L1, DSL::SingleLinkedList<T> &L2, DSL::SingleLinkedList<T> &L3){
+	auto f = [&](const T& val) {
+		if(DSL::contains(L2.begin(), L2.end(), val) && !DSL::contains(L3.begin(), L3.end(), val)){
+			L3.append(val);
+		}
+	};
+	DSL::for_each(L1.begin(), L1.end() , f);
+
+}
+
+
+template<typename UnionFunction>
+void testIntersection(const int SIZE, UnionFunction uf ){
+	//unione of two equal lists = one list
+		DSL::SingleLinkedList<int> L1;
+		initializeList(SIZE,0,L1);
+		DSL::SingleLinkedList<int> L2;
+		initializeList(SIZE,0,L2);
+		DSL::SingleLinkedList<int> L3;
+
+		ASSERT_EQ(false,L3==L1);
+		ASSERT_EQ(false,L3==L2);
+		uf(L1,L2,L3);
+		ASSERT_EQ(true,L3==L1);
+		ASSERT_EQ(true,L3==L2);
+		ASSERT_EQ(SIZE,L3.size());
+
+		L1.clear();
+		L3.clear();
+		initializeList(SIZE,SIZE,L1);
+		//now l1 and L2 have empty intersection
+		uf(L1,L2,L3);
+//	DSL::print(L1.begin(), L1.end());
+//DSL::print(L2.begin(), L2.end());
+//	DSL::print(L3.begin(), L3.end());
+		ASSERT_EQ(true,L3 !=L1 );
+		ASSERT_EQ(true,L3 !=L2 );
+		ASSERT_EQ(L3.isEmpty(),true);
+
+		//intersection not empty
+		L1.clear();
+		L3.clear();
+		initializeList(SIZE,SIZE/2,L1);
+		uf(L1,L2,L3);
+		ASSERT_EQ(SIZE/2,L3.size());
+
+
+}
+
+
+
+TEST(LinkedList,LinkedList_INTERSECTION_naive){
+	testIntersection(10, listIntersectionNaive<int>);
+}
+TEST(LinkedList,LinkedList_INTERSECTION_smarter){
+//	testIntersection(10000, listIntersectionSmarter<int>);
 }
